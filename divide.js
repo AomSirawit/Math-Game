2eb072c43c39d7d2d8f8d322e2f08929c4e5fe12
AomSirawit <makdeesirawit@gmail.com>
2023-03-29 19:52:50 +0700

    deploy
    deploy

---
+++ divide.js
@@ -0,0 +1,29 @@
+//random number
+let n1 = Math.floor(Math.random()*20+1);
+let n2 = Math.floor(Math.random()*20+1);
+
+document.getElementById("intext1").value = n1;
+document.getElementById("intext2").value = n2;
+//calculate answer
+let answer = n1 / n2;
+function checkAnswer(){
+    var user = document.getElementById("answer").value;
+
+    if( user == answer){
+        document.getElementById("result").innerHTML = "คำตอบถูกต้อง!!!";
+
+    }else{
+        document.getElementById("result").innerHTML = "คำตอบผิด!!!"+ " คำตอบที่ถูกคือ "+ answer.toFixed(2);
+
+    }
+    var user = document.getElementById("answer").value = "";
+    n1 = Math.floor(Math.random()*20+1);
+    n2 = Math.floor(Math.random()*20+1);
+
+    document.getElementById("intext1").value = n1;
+    document.getElementById("intext2").value = n2;
+
+    answer = n1 / n2;
+    
+}
+    
